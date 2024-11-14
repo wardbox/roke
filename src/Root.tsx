cfe721d74eb98f9e390ff421e094784a4cfde65b
wardbox <31261302+wardbox@users.noreply.github.com>
2024-11-14 10:40:44 -0800

    Add VSCode launch config and update nav component (#2)
    - Added `.vscode/launch.json` for running the "wasp start" script in a node terminal.
    - Updated `nav.tsx` to remove logo prop, use new icon (Mountains), and simplify code structure.
    - Removed unused `rokeLogo` import from `Root.tsx`.
    - Deleted `roke-logomark.svg`.

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,7 +5,6 @@ import '@fontsource-variable/public-sans'
 import { ThemeProvider } from './components/theme-provider'
 import { Toaster } from './components/ui/toaster'
 import { Nav } from './components/ui/nav'
-import rokeLogo from './static/roke-logomark.svg'
 import { useAuth } from 'wasp/client/auth'
 import { Footer } from './components/ui/footer'
 import { MotionConfig, useReducedMotion } from "motion/react"
@@ -20,7 +19,7 @@ export default function Root() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="flex flex-col h-screen">
           <header className='border-b'>
-            <Nav logo={rokeLogo} user={user} userLoading={isLoading} />
+            <Nav user={user} userLoading={isLoading} />
           </header>
           <main className="mb-auto p-12 w-full max-w-7xl mx-auto">
             <Outlet />
